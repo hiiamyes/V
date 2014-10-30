c8eac09bb8e19ceb26331b3ae68afc36a1bcf89b
Yes <joshuayes@storysensecomputing.com>
2014-10-30 09:37:21 +0800

    Enable push notification.

--- app/src/main/java/tw/yes/v/AppApplication.java
+++ app/src/main/java/tw/yes/v/AppApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.parse.Parse;
 import com.parse.ParseObject;
+import com.parse.ParsePush;
 
 import tw.yes.v.model.Candidate;
 
@@ -16,5 +17,9 @@ public class AppApplication extends Application {
         ParseObject.registerSubclass(Candidate.class);
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "CK3JOXdcsoOt1fCk4VG4bbM09nmh2bdUnuuVB5kk", "F4i7OkP4eoiXXWustFp2OGYAUjfgV5SVncJF3T2C");
+
+        ParsePush.subscribeInBackground("yes");
+        ParsePush.subscribeInBackground("");
+
     }
 }
